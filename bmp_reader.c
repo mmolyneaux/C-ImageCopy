718efcb3ba0dcb4f7e61314e595060bbf622a034
mmolyneaux <m_molyneaux@hotmail.com>
2025-03-24 23:01:12 -0600

    Created bmp_reader.h

--- bmp_reader.c
+++ bmp_reader.c
@@ -2,34 +2,9 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "bmp_reader.h"
 
-#pragma pack(push, 1) // Ensure no padding in structs
 
-typedef struct {
-    uint16_t type;
-    uint32_t size;
-    uint16_t reserved1;
-    uint16_t reserved2;
-    uint32_t offset_bits;
-
-} BM_File_Header;
-
-typedef struct {
-    uint32_t size;
-    int32_t width;
-    int32_t height;
-    uint16_t planes;
-    uint16_t bit_count;
-    uint32_t compression;
-    uint32_t size_image;
-    int32_t x_pixels_per_meter;
-    int32_t y_pixels_per_meter;
-    uint32_t clr_used;
-    uint32_t clr_important;
-
-} BM_Info_Header;
-
-#pragma pack(pop)
 
 void read_bitmap_file(char *input_file_name, BM_File_Header *file_header,
                       BM_Info_Header *info_header) {
---
+++ bmp_reader.h
@@ -0,0 +1,46 @@
+#ifndef BITMAP_H
+#define BITMAP_H
+
+#include <stdint.h>
+
+#pragma pack(push, 1) // Ensure no padding in structs
+
+typedef struct {
+    uint16_t type;
+    uint32_t size;
+    uint16_t reserved1;
+    uint16_t reserved2;
+    uint32_t offset_bits;
+
+} BM_File_Header;
+
+typedef struct {
+    uint32_t size;
+    int32_t width;
+    int32_t height;
+    uint16_t planes;
+    uint16_t bit_count;
+    uint32_t compression;
+    uint32_t size_image;
+    int32_t x_pixels_per_meter;
+    int32_t y_pixels_per_meter;
+    uint32_t clr_used;
+    uint32_t clr_important;
+
+} BM_Info_Header;
+
+#pragma pack(pop)
+
+typedef struct {
+    BM_File_Header file_header;
+    BM_Info_Header info_header;
+    uint8_t *pixel_data;
+
+} Bitmap;
+
+// Function prototypes
+int read_bitmap(const char *filename, Bitmap *bmp);
+int write_bitmap(const char *filename, const Bitmap *bmp);
+void freeBitmap(Bitmap *bmp);
+
+#endif // BITMAP_H
